dd9c6ae633edca158878ae07d72a3cf14b040079
Niran Babalola <niran@niran.org>
2023-11-23 13:28:13 -0600

    Add an EBS volume to RiversideUtility

--- riverside-cloud/main.tf
+++ riverside-cloud/main.tf
@@ -12,11 +12,29 @@ provider "aws" {
 }
 
 resource "aws_instance" "riverside_utility" {
-  ami           = "ami-0e83be366243f524a"
-  instance_type = "t3.medium"
+  availability_zone = "us-east-2b"
+  ami               = "ami-0e83be366243f524a" # Ubuntu Server 22.04 LTS
+  instance_type     = "t3.medium"
 
   tags = {
     Name = "RiversideUtility"
     Description = "Primary utility server for the riverside environment"
   }
+}
+
+resource "aws_ebs_volume" "riverside_utility" {
+  availability_zone = "us-east-2b"
+  size              = 20
+  encrypted         = true
+  type              = "gp3"
+
+  tags = {
+    Name = "RiversideUtilityVolume"
+  }
+}
+
+resource "aws_volume_attachment" "riverside_utility" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.riverside_utility.id
+  instance_id = aws_instance.riverside_utility.id
 }
